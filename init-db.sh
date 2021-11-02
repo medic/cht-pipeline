8968654ca29bd367e65b954f79a31d2b95ffdc8c
Aidan Macdonald <aidan@medic.org>
2021-11-02 14:23:26 +0200

    Adding pipeline

---
+++ init-db.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE TABLE public.$POSTGRES_TABLE (
+        "@version" TEXT,
+        "@timestamp" TIMESTAMP,
+        "_id" TEXT,
+        "_rev" TEXT,
+        doc jsonb,
+        doc_as_upsert BOOLEAN,
+        UNIQUE ("_id", "_rev")
+    );
+EOSQL
