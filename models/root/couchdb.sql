c8580c742b907769da8d71c1dd5d49c44bb5b2d8
Aidan Macdonald <aidan@medic.org>
2021-12-08 08:35:55 -0500

    Separating out sync elements

--- models/root/couchdb.sql
+++ models/root/couchdb.sql
@@ -10,4 +10,4 @@
 SELECT
     doc->>'type' AS type,
     *
-FROM {{ env_var('POSTGRES_SCHEMA') }}.{{ env_var('POSTGRES_TABLE') }}
+FROM {{ env_var('ROOT_POSTGRES_SCHEMA') }}.{{ env_var('POSTGRES_TABLE') }}
