8d4cad65d3597601ac168179b3f3a5efc194c859
MM <wambua@medicmobile.org>
2022-03-08 23:50:47 +0300

    Add views and mat views

---
+++ models/brac/contact_views/contactview_person_fields.sql
@@ -0,0 +1,8 @@
+SELECT person.uuid,
+		person.phone,
+		person.phone2,
+		person.date_of_birth,
+		parent.type AS parent_type
+	FROM {{ ref("contactview_metadata") }} person
+	LEFT JOIN {{ ref("contactview_metadata") }} parent ON person.parent_uuid = parent.uuid
+	WHERE person.type = 'person'::text;
---
+++ models/brac/contact_views/form_metadata.sql
@@ -0,0 +1,33 @@
+{{
+    config(
+        materialized = 'view',
+        unique_key="uuid"
+        indexes=[
+            {'columns': ['reported']},
+            {'columns': ['reported_by']},
+            {'columns': ['chw']},
+            {'columns': ['reported_by_parent']},
+            {'columns': ['patient_id']},
+            {'columns': ['form']},
+            {'columns': ['formname']}            
+        ]
+    )
+}}
+SELECT
+        doc ->> '_id' AS uuid,
+        doc #>> '{contact,_id}' AS reported_by,
+        doc #>> '{contact,_id}' AS chw,
+        doc #>> '{contact,parent,_id}' AS reported_by_parent,
+        COALESCE(doc->>'patient_id',doc #>> '{fields,patient_id}') AS patient_id,
+        doc ->> 'form' AS form,
+        doc ->> 'form' AS formname,
+        COALESCE((doc ->> 'errors'),'[]') != '[]' AS errors,
+        to_timestamp((NULLIF(doc ->> 'reported_date', '')::bigint / 1000)::double precision) AS reported
+        
+    FROM
+        {{ ref("couchdb") }} 
+        
+    WHERE
+        (doc ->> 'type') = 'data_record'
+        AND (doc #>> '{contact,_id}') IS NOT NULL
+        AND (doc ->> 'form') IS NOT NULL
