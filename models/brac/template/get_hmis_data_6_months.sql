cfe966c9dc3753b0fed7f6b0d5dfb4c572101219
MM <wambua@medicmobile.org>
2022-03-18 23:59:23 +0300

    Add tz

--- models/brac/template/get_hmis_data_6_months.sql
+++ models/brac/template/get_hmis_data_6_months.sql
@@ -1,6 +1,6 @@
-{% set time_now =  dbt_date.now() %} 
+{% set time_now =  dbt_date.now("America/New_York") %} 
 
-{% set time_six_months_ago = dbt_date.n_months_ago(6) %}
+{% set time_six_months_ago = dbt_date.n_months_ago(6, tz="America/New_York") %}
 
 {% set results = time_six_months_ago %}
 
