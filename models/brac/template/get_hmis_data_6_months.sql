3ec0cf0cbc41218d6973f8bcecaa407e0b2c8697
MM <wambua@medicmobile.org>
2022-03-18 21:40:17 +0300

    Edit now()

--- models/brac/template/get_hmis_data_6_months.sql
+++ models/brac/template/get_hmis_data_6_months.sql
@@ -1,10 +1,14 @@
 {% set six_months_ago %}
 SELECT
 
-{{get_hmis_data(('now'::timestamp), ('now'::timestamp - '6 month'::interval))}}
+{{get_hmis_data((date_trunc('day', NOW())), (date_trunc('day', NOW() - interval '6 month')))}}
 
 {% endset %}
 
 {% set results = run_query(six_months_ago) %}
 
 {{ log(results, info=True) }}
+
+{{ return([]) }}
+
+{% endset %}
