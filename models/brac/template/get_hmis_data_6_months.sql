351e30f98b0e2faeee564a78edf938c17154a7a5
MM <wambua@medicmobile.org>
2022-03-18 23:47:42 +0300

    log

--- models/brac/template/get_hmis_data_6_months.sql
+++ models/brac/template/get_hmis_data_6_months.sql
@@ -1,1 +1,11 @@
 {% set time_now =  dbt_date.now() %} 
+
+{% set time_six_months_ago = dbt_date.n_months_ago(6) %}
+
+{% set results = time_six_months_ago %}
+
+{{ log(results, info=True) }}
+
+{{ return([]) }}
+
+{% endset %}
