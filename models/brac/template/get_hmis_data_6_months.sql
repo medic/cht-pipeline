8b38272036d124eec5aca8f10c24189587198327
MM <wambua@medicmobile.org>
2022-03-19 00:16:57 +0300

    time_now

--- models/brac/template/get_hmis_data_6_months.sql
+++ models/brac/template/get_hmis_data_6_months.sql
@@ -1,7 +1,7 @@
-{% set time_now =  {{ dbt_date.now("America/New_York") }} %} 
+{% set time_now =  dbt_date.now("America/New_York") %} 
 
-{% set time_six_months_ago = {{ dbt_date.n_months_ago(6, tz="America/New_York") }} %}
+{% set time_six_months_ago = dbt_date.n_months_ago(6, tz="America/New_York") %}
 
-{% set results = time_six_months_ago %}
+{% set results = time_now %}
 
 {{ log(results, info=True) }}
