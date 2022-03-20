dc4fd172f3d70c797a8f6a5495d4f30f012915cf
MM <wambua@medicmobile.org>
2022-03-20 22:49:36 +0300

    SQL

--- models/brac/template/get_hmis_data_6_months.sql
+++ models/brac/template/get_hmis_data_6_months.sql
@@ -1,6 +1,6 @@
-{% set time_now =  dbt_utils.current_timestamp() -%} 
+{% set time_now =  dbt_utils.get_query_results_as_dict("SELECT DATE_TRUNC('month', NOW())") -%} 
 
-{% set time_six_months_ago = dbt_utils.current_timestamp() -%}
+{% set time_six_months_ago = dbt_utils.get_query_results_as_dict("SELECT DATE_TRUNC('month', NOW() - interval '6 month')") -%}
 
 {% set results = time_now %}
 
