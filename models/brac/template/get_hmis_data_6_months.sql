6094517b90935f9a86828b1adb82bd16bd2b86f6
MM <wambua@medicmobile.org>
2022-03-21 22:35:28 +0300

    Change to \set

--- models/brac/template/get_hmis_data_6_months.sql
+++ models/brac/template/get_hmis_data_6_months.sql
@@ -2,15 +2,9 @@
 
 \set time_six_months_ago (date_trunc('day', NOW() - interval '6 month'))
 
-{% set six_months_ago %}
+\set six_months_ago = SELECT {{ get_hmis_data(time_now, time_six_months_ago) }}
 
-SELECT
-
-{{ get_hmis_data(time_now, time_six_months_ago) }}
-
-{% endset %}
-
-{% set results = run_query(six_months_ago) %}
+\set results = run_query(six_months_ago)
 
 {{ log(results, info=True) }}
 
