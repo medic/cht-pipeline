0b2e76a91ae19bcadeb07927828962ac293585d1
MM <wambua@medicmobile.org>
2022-03-21 22:31:00 +0300

    Try \set

--- models/brac/template/get_hmis_data_6_months.sql
+++ models/brac/template/get_hmis_data_6_months.sql
@@ -1,7 +1,19 @@
-{% set time_now =  dbt_utils.get_query_results_as_dict("SELECT DATE_TRUNC('month', NOW())") -%} 
+\set time_now NOW()
 
-{% set time_six_months_ago = dbt_utils.get_query_results_as_dict("SELECT DATE_TRUNC('month', NOW() - interval '6 month')") -%}
+\set time_six_months_ago (date_trunc('day', NOW() - interval '6 month'))
 
-{% set results = time_now %}
+{% set six_months_ago %}
+
+SELECT
+
+{{ get_hmis_data(:time_now, :time_six_months_ago) }}
+
+{% endset %}
+
+{% set results = run_query(six_months_ago) %}
 
 {{ log(results, info=True) }}
+
+{{ return([]) }}
+
+{% endset %}
