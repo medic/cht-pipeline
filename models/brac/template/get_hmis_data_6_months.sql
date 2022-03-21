5d6bf7f345aea31d1a9c65784fbae09a8161f679
MM <wambua@medicmobile.org>
2022-03-21 23:33:13 +0300

    NOW() direct

--- models/brac/template/get_hmis_data_6_months.sql
+++ models/brac/template/get_hmis_data_6_months.sql
@@ -2,7 +2,7 @@
 
 \set time_six_months_ago (date_trunc('day', NOW() - interval '6 month'))
 
-\set six_months_ago = SELECT {{ get_hmis_data(time_now, time_six_months_ago) }}
+\set six_months_ago = SELECT {{ get_hmis_data(NOW(), (date_trunc('day', NOW() - interval '6 month'))) }}
 
 \set results = run_query(six_months_ago)
 
