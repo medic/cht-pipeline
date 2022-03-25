ef95d995e3051a839fbdf405d9829f13aedeb711
MM <wambua@medicmobile.org>
2022-03-25 13:14:47 +0300

    Add assessment macro

---
+++ models/brac/template/get_assessment_1_month.sql
@@ -0,0 +1,15 @@
+{% set time_now = dbt_utils.current_timestamp() -%}
+
+
+{% set time_six_months_ago = dbt_date.n_months_ago(2) -%}
+
+{% set get_hmis_data_query = get_hmis_data(
+    startDate=time_six_months_ago, 
+    endDate=time_now
+)
+
+-%}
+
+{% set results = run_query(get_hmis_data_query) %}
+
+{{ log(results, info=True) }}
