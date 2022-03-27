f5acb2ca8411e30a5ae9499eb749eb5ea0388b4f
MM <wambua@medicmobile.org>
2022-03-28 00:06:06 +0300

    Make get_asssessment incremental

--- models/brac/template/get_assessment_1_month.sql
+++ models/brac/template/get_assessment_1_month.sql
@@ -1,22 +1,20 @@
+{{
+    config(
+        materialized = 'incremental',
+        unique_key='uuid',
+        indexes=[
+            {'columns': ['reported_date']},
+            {'columns': ['reported_by_parent']}
+        ]
+    )
+}}
+
 {% set time_now = dbt_utils.current_timestamp() -%}
 
 
 {% set time_six_months_ago = dbt_date.n_months_ago(2) -%}
 
-{% set get_assessment_query = get_assessment_data(
+{{get_assessment_data(
     startDate=time_six_months_ago, 
     endDate=time_now
-)
-
--%}
-
-{% set results = run_query(get_assessment_query) %}
-
-{% if execute %}
-{# Return the first column #}
-{% set uuid_results_list = results.columns[0].values() %}
-{% else %}
-{% set uuid_results_list = [] %}
-{% endif %}
-
-{{ log(uuid_results_list, info=True) }}
+)}}
