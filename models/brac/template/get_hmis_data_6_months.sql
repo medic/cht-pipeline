1a3af4dce30b3c69f4e067508ded65a8e1c4bdc5
MM <wambua@medicmobile.org>
2022-03-28 11:17:58 +0300

    Add get_hmis_data_6_months back

--- models/brac/template/get_hmis_data_6_months.sql
+++ models/brac/template/get_hmis_data_6_months.sql
@@ -3,13 +3,7 @@
 
 {% set time_six_months_ago = dbt_date.n_months_ago(6) -%}
 
-{% set get_hmis_data_query = get_hmis_data(
+{{ get_hmis_data_query = get_hmis_data(
     startDate=time_six_months_ago, 
     endDate=time_now
-)
-
--%}
-
-{% set results = run_query(get_hmis_data_query) %}
-
-{{ log(results, info=True) }}
+)}}
