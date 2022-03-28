8fb2e54dabebe9c5a28950305290366d181eeb71
MM <wambua@medicmobile.org>
2022-03-28 12:03:19 +0300

    Remove query(

--- models/brac/template/get_hmis_data_6_months.sql
+++ models/brac/template/get_hmis_data_6_months.sql
@@ -15,7 +15,7 @@
 
 {% set time_six_months_ago = dbt_date.n_months_ago(6) -%}
 
-{{ get_hmis_data_query = get_hmis_data(
+{{ get_hmis_data(
     startDate=time_six_months_ago, 
     endDate=time_now
 )}}
