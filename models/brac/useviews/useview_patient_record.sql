77216e73a7fd6a651767d49e4b3712a5ad998983
MM <wambua@medicmobile.org>
2022-04-27 21:37:40 +0300

    useview_patient_record materialized view

--- models/brac/useviews/useview_patient_record.sql
+++ models/brac/useviews/useview_patient_record.sql
@@ -1,4 +1,16 @@
-SELECT 	
+{{
+    config(
+        materialized = 'view',
+        unique_key='idx_useview_patient_record_reported_patient_id'
+    )
+}}
+
+SELECT
+{{ dbt_utils.surrogate_key('reported', 'patient_id') }} AS useview_assessment_reported_age_uuid,
+*
+FROM(
+SELECT 
+    	
 	to_timestamp((doc#>>'{reported_date}')::double precision/1000)::timestamp AS reported,
 	doc#>>'{fields,patient_id}' AS patient_id,
 	doc->>'form' AS form,
@@ -8,3 +20,4 @@ FROM
 	{{ ref("couchdb") }}
 WHERE 
 	doc#>'{fields}' ? 'patient_id'
+) x
