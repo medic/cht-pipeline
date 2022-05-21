ea1e270ee5b969ca20a8dd81dff8d0a5dbae00e4
MM <wambua@medicmobile.org>
2022-05-21 13:53:38 +0300

    quotes

--- models/brac/useviews/useview_assessment.sql
+++ models/brac/useviews/useview_assessment.sql
@@ -89,5 +89,5 @@ FROM(
 ) x
 
 {% if is_incremental() %}
-    AND COALESCE("reported" > (SELECT MAX("reported") FROM {{ this }}), True)
+    AND COALESCE(reported > (SELECT MAX(reported) FROM {{ this }}), True)
 {% endif %}
