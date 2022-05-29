a55942891bf6277eb2935c5c09a3b8a92d4af290
MM <wambua@medicmobile.org>
2022-05-29 21:00:02 +0300

    @timestamp raw_contacts

--- models/contact_views/raw_contacts.sql
+++ models/contact_views/raw_contacts.sql
@@ -1,4 +1,6 @@
-SELECT couchdb.doc
+SELECT 
+couchdb.doc, 
+"@timestamp"::timestamp without time zone AS "@timestamp"
 FROM {{ ref("couchdb") }}
 WHERE (couchdb.doc ->> 'type'::text) = ANY
       (ARRAY ['contact'::text, 'clinic'::text, 'district_hospital'::text, 'health_center'::text, 'person'::text])
