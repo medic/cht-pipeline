24963b2f16fa0ded76a89d552cf86834737d2bd8
MM <wambua@medicmobile.org>
2022-06-29 19:32:14 +0300

    Change contactview_family

--- models/brac/contact_views/contactview_family.sql
+++ models/brac/contact_views/contactview_family.sql
@@ -3,9 +3,9 @@ SELECT
   contactview_clinic.name,
   contactview_clinic.chw_uuid,
   contactview_clinic.created,
-  contactview_family_survey.mosquito_nets,
-  contactview_family_survey.hygeinic_toilet,
-  contactview_family_survey.family_planning_method,
-  contactview_family_survey.source_of_drinking_water
+  contactview_family_survey.solar_light,
+  contactview_family_survey.water_filter,
+  contactview_family_survey.children_under_5,
+  contactview_family_survey.improved_cook_stove
 FROM {{ ref("contactview_clinic") }}
 LEFT JOIN {{ ref("contactview_family_survey") }} ON contactview_clinic.uuid = contactview_family_survey.uuid
