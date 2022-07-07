a12c0f65726cfb4956e82d373bcd15501b6b2890
MM <wambua@medicmobile.org>
2022-07-07 23:49:45 +0300

    Update

--- models/brac/useviews/useview_visit.sql
+++ models/brac/useviews/useview_visit.sql
@@ -27,8 +27,34 @@ SELECT
 		to_timestamp((NULLIF(doc ->> 'reported_date', '')::bigint / 1000)::double precision) AS reported
 				
 	FROM
+		 dbt.couchdb
+			
+	WHERE doc ->> 'form' IN ('pregnancy_visit', 'postnatal_care', 'immunization_follow_up')
+
+UNION ALL 
+
+SELECT
+        "@timestamp"::timestamp without time zone AS "@timestamp",
+		(doc ->> '_id') AS uuid,
+		doc->>'form' AS form,
+		COALESCE((doc #>> '{fields,inputs,source_id}'),'') AS source_id,
+		CASE
+			WHEN doc ->> 'form' = 'pregnancy_visit' THEN 'anc'
+			WHEN doc ->> 'form' =  'postnatal_care' THEN 'pnc'
+			WHEN doc ->> 'form' = 'assessment' THEN 'iccm'
+			WHEN doc ->> 'form' = 'immunization_follow_up' THEN 'imm'
+		END AS visit_type,		
+		NOT ((doc #>> '{fields,danger_signs}') IS NULL OR (doc #>> '{fields,danger_signs}') = '') AS danger_signs,							
+		COALESCE((doc #>> '{fields,patient_id}'),'') AS patient_id,												
+		COALESCE((doc #>> '{contact,_id}'),'') AS reported_by,
+		COALESCE((doc #>> '{contact,parent,_id}'),'') AS reported_by_parent,
+		to_timestamp((NULLIF(doc ->> 'reported_date', '')::bigint / 1000)::double precision) AS reported
+				
+	FROM
 		 {{ ref("couchdb") }}
 			
-	WHERE doc ->> 'form'::text = ANY (ARRAY ['pregnancy_visit'::text, 'postnatal_care'::text, 'immunization_follow_up'::text])
-		OR (doc ->> 'form' = 'assessment' AND (doc #>> '{fields,patient_age_in_years}')::int <= 5)
+	WHERE (doc ->> 'form' = 'assessment' AND (nullif(doc #>> '{fields,patient_age_in_years}', ''))::int <= 5)
 
+{% if is_incremental() %}
+        AND "@timestamp" > {{ max_existing_timestamp('"@timestamp"') }}
+{% endif %}
