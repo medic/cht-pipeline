97d8a0fd6f7c40882e03364cfa93ab3a6e17e7a2
MM <wambua@medicmobile.org>
2022-07-19 21:39:56 +0300

    union to union all

--- models/brac/views/hhview_visits.sql
+++ models/brac/views/hhview_visits.sql
@@ -1,3 +1,5 @@
+SELECT reported, reported_by_parent, household_id FROM(
+
 SELECT 
 	upr.reported AS reported,
 	upr.reported_by_parent,
@@ -5,14 +7,14 @@ SELECT
 FROM 
 	{{ ref("useview_patient_record") }} AS upr
 INNER JOIN {{ ref("contactview_person") }} cvp ON (upr.patient_id=cvp.patient_id)
-UNION 
+UNION ALL
 SELECT 
 	reported,
 	reported_by_parent,
 	place_id AS household_id
 FROM 
 	{{ ref("useview_place_record") }}
-UNION
+UNION ALL
 SELECT 
 	reported::timestamp as reported,
 	parent_uuid AS reported_by_parent,
@@ -21,3 +23,5 @@ FROM
 	{{ ref("contactview_metadata") }} 
 WHERE 
 	type='clinic'
+)
+GROUP BY reported, reported_by_parent, household_id
