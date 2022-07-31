b4daaa5b0a111c9e42e10da551947eadc9a012d3
MM <wambua@medicmobile.org>
2022-07-31 17:59:28 +0300

    Indentation

--- models/brac/views/impactview_month.sql
+++ models/brac/views/impactview_month.sql
@@ -1,5 +1,5 @@
 SELECT 
-	 	generate_series(date_trunc('month', now() - '1 year'::interval), now(), '1 mon'::interval)::date AS month,
-	    date_part('epoch', generate_series(date_trunc('month', now() - '1 year'::interval), now(), '1 mon'::interval)::date) AS epoch
-	ORDER BY
-		epoch
+    generate_series(date_trunc('month', now() - '1 year'::interval), now(), '1 mon'::interval)::date AS month,
+    date_part('epoch', generate_series(date_trunc('month', now() - '1 year'::interval), now(), '1 mon'::interval)::date) AS epoch
+ORDER BY
+    epoch
