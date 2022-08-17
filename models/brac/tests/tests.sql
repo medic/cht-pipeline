8d3295d62cb66418d0a6c9a44749274842bf7cee
MM <wambua@medicmobile.org>
2022-08-17 23:36:38 +0300

    rm tests

--- models/brac/tests/tests.sql
+++
@@ -1,6 +0,0 @@
-{{ config(materialized='raw_sql') }}
-
-SELECT i.uuid FROM dblink('dbname=brac-ug-dev user=brac_ug_dblink password=Bc5Tty14vQaxAbw', 
-                                'SELECT couchdb.doc FROM v1.couchdb LIMIT 1
-                        ') 
-i (uuid TEXT);
