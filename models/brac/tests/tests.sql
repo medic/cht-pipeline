87716bae230a559fb69875b74664ef6389919542
MM <wambua@medicmobile.org>
2022-08-17 23:29:44 +0300

    rm host

--- models/brac/tests/tests.sql
+++ models/brac/tests/tests.sql
@@ -1,6 +1,6 @@
 {{ config(materialized='raw_sql') }}
 
-SELECT i.uuid FROM dblink('dbname=brac-ug-dev port=5432 host=localhost user=brac_ug_dblink password=Bc5Tty14vQaxAbw', 
+SELECT i.uuid FROM dblink('dbname=brac-ug-dev user=brac_ug_dblink password=Bc5Tty14vQaxAbw', 
                                 'SELECT couchdb.doc FROM v1.couchdb LIMIT 1
                         ') 
 i (uuid TEXT);
