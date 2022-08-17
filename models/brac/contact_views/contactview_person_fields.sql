9a97182b48dbadfe10b3597b06d34d8aa056a9eb
MM <wambua@medicmobile.org>
2022-08-17 23:49:02 +0300

    person

--- models/brac/contact_views/contactview_person_fields.sql
+++ models/brac/contact_views/contactview_person_fields.sql
@@ -3,7 +3,7 @@ SELECT person.uuid,
 		person.phone2,
 		person.date_of_birth,
 		parent.type AS parent_type,
-		contactview_metadata."@timestamp" AS "@timestamp"
+		person."@timestamp" AS "@timestamp"
 	FROM {{ ref("contactview_metadata") }} person
 	LEFT JOIN {{ ref("contactview_metadata") }} parent ON person.parent_uuid = parent.uuid
 	WHERE person.type = 'person'::text
