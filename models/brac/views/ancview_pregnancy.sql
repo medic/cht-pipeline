f3e12a751740ae7e57038d168ccb79d885c8f25e
MM <wambua@medicmobile.org>
2022-09-15 18:51:41 +0300

    WHERE --> AND

--- models/brac/views/ancview_pregnancy.sql
+++ models/brac/views/ancview_pregnancy.sql
@@ -41,5 +41,5 @@
     INNER JOIN {{ ref("contactview_metadata") }} contact ON contact.uuid = preg.chw
 
     {% if is_incremental() %}
-        AND preg . "@timestamp" > {{ max_existing_timestamp('"@timestamp"', target_ref=ref("useview_pregnancy")) }}
+        WHERE preg . "@timestamp" > {{ max_existing_timestamp('"@timestamp"', target_ref=ref("useview_pregnancy")) }}
 {% endif %}
