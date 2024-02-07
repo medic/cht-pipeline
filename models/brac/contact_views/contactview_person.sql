51d65b166da1c13352277cd65f17f12fe9c22fef
Njuguna Ndung'u <njuguna@medic.org>
2024-02-08 00:52:05 +0300

    fix: try fixing CAST error

--- models/brac/contact_views/contactview_person.sql
+++ models/brac/contact_views/contactview_person.sql
@@ -27,12 +27,5 @@ SELECT
 		doc->>'type' = 'person'
 
         {% if is_incremental() %}
-    		AND (
-        		"@timestamp" > 
-				{% if max_existing_timestamp('"@timestamp"') == none %}
-					'1970-01-01 00:00:00'::timestamp
-				{% else %}
-					{{ max_existing_timestamp('"@timestamp"') }}
-				{% endif %}
-			)
+			AND "@timestamp" > {{ coalesce(max_existing_timestamp('"@timestamp"'), '1970-01-01 00:00:00'::timestamp) }}
 		{% endif %}
