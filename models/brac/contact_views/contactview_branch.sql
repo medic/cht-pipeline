d3b69e329f8a2f039a4d61505372c5b79365b41c
Njuguna Ndung'u <njuguna@medic.org>
2024-02-07 21:11:07 +0300

    feat: convert contactview_branch into incremental model

--- models/brac/contact_views/contactview_branch.sql
+++ models/brac/contact_views/contactview_branch.sql
@@ -1,8 +1,23 @@
+{{
+    config(
+        materialized = 'incremental',
+        indexes=[
+            {'columns': ['uuid']},
+            {'columns': ['name']}
+        ]
+    )
+}}
+
 SELECT
     contactview_hospital.uuid,
     contactview_hospital.name,
     couchdb.doc->>'area' AS area,
     couchdb.doc->>'region' AS region
+    couchdb.doc->>'"@timestamp' AS @timestamp
 FROM
     {{ ref("contactview_hospital") }}
     INNER JOIN {{ ref("couchdb") }} ON (couchdb.doc ->> '_id'::text = contactview_hospital.uuid AND couchdb.doc ->> 'type' = 'district_hospital')
+
+    {% if is_incremental() %}
+        WHERE contactview_chw."@timestamp" > {{ max_existing_timestamp('"@timestamp"', target_ref=ref("contactview_chw")) }}
+    {% endif %}
