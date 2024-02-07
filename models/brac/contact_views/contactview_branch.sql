9f7176e1eeee8e81df0d504e6d86cda8e9a9ef77
Njuguna Ndung'u <njuguna@medic.org>
2024-02-07 23:40:53 +0300

    feat: try new way of selecting timestamp

--- models/brac/contact_views/contactview_branch.sql
+++ models/brac/contact_views/contactview_branch.sql
@@ -13,11 +13,11 @@ SELECT
     contactview_hospital.name,
     couchdb.doc->>'area' AS area,
     couchdb.doc->>'region' AS region,
-    couchdb.doc->>'@timestamp' AS @timestamp
+    "couchdb.@timestamp"::timestamp without time zone AS "@timestamp"
 FROM
     {{ ref("contactview_hospital") }}
     INNER JOIN {{ ref("couchdb") }} ON (couchdb.doc ->> '_id'::text = contactview_hospital.uuid AND couchdb.doc ->> 'type' = 'district_hospital')
 
     {% if is_incremental() %}
-        WHERE contactview_chw."@timestamp" > {{ max_existing_timestamp('"@timestamp"', target_ref=ref("contactview_chw")) }}
+        WHERE contactview_branch."@timestamp" > {{ max_existing_timestamp('"@timestamp"', target_ref=ref("contactview_chw")) }}
     {% endif %}
