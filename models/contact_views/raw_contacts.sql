01e11ba49ad2d4df7e4e580eb796f3526ae74354
Njuguna Ndung'u <njuguna@medic.org>
2024-04-03 17:04:11 +0300

    feat: change raw_contacts to incremental table

--- models/contact_views/raw_contacts.sql
+++ models/contact_views/raw_contacts.sql
@@ -1,4 +1,16 @@
-SELECT couchdb.doc
+{{ 
+      config(
+            materialized='incremental',
+      )
+ }}
+
+SELECT
+      couchdb.doc
+      coucdb."@timestamp"
 FROM {{ ref("couchdb") }}
 WHERE (couchdb.doc ->> 'type'::text) = ANY
       (ARRAY ['contact'::text, 'clinic'::text, 'district_hospital'::text, 'health_center'::text, 'person'::text])
+
+{% if is_incremental() %}
+  WHERE "@timestamp" >= (SELECT MAX("@timestamp") FROM {{ this }})
+{% endif %}
