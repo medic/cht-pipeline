22f0f9a9177a3e43fe8162eafc21715061cbaac8
Njuguna Ndung'u <njuguna@medic.org>
2024-04-03 17:08:08 +0300

    chore: fix typo

--- models/contact_views/raw_contacts.sql
+++ models/contact_views/raw_contacts.sql
@@ -6,7 +6,7 @@
 
 SELECT
       couchdb.doc
-      coucdb."@timestamp"
+      couchdb."@timestamp"
 FROM {{ ref("couchdb") }}
 WHERE (couchdb.doc ->> 'type'::text) = ANY
       (ARRAY ['contact'::text, 'clinic'::text, 'district_hospital'::text, 'health_center'::text, 'person'::text])
