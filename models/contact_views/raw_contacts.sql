6d83d6b293898c46cccd7d0330dedd994859391d
Njuguna Ndung'u <njuguna@medic.org>
2024-04-04 14:16:07 +0300

    feat: remove addional WHERE clause

--- models/contact_views/raw_contacts.sql
+++ models/contact_views/raw_contacts.sql
@@ -12,5 +12,5 @@ WHERE (couchdb.doc ->> 'type'::text) = ANY
       (ARRAY ['contact'::text, 'clinic'::text, 'district_hospital'::text, 'health_center'::text, 'person'::text])
 
 {% if is_incremental() %}
-  WHERE couchdb."@timestamp" >= (SELECT MAX(couchdb."@timestamp") FROM {{ this }})
+  AND couchdb."@timestamp" >= (SELECT MAX(couchdb."@timestamp") FROM {{ this }})
 {% endif %}
