11e079dc17ffd3ce4b182761465fd1ccb6a801a4
Njuguna Ndung'u <njuguna@medic.org>
2024-04-05 13:21:17 +0300

    chore: typo

--- models/brac/contact_views/contactview_person_fields.sql
+++ models/brac/contact_views/contactview_person_fields.sql
@@ -10,7 +10,7 @@ SELECT
   person.phone2,
   person.date_of_birth,
   person.reported,
-  person."@timestamp"
+  person."@timestamp",
   parent.type AS parent_type
 FROM {{ ref("contactview_metadata") }} person
 LEFT JOIN {{ ref("contactview_metadata") }} parent ON person.parent_uuid = parent.uuid
