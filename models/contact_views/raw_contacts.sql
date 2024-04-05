c69a86b4e6f4382c4d1ea4694e92ffdc78a60f41
Njuguna Ndung'u <njuguna@medic.org>
2024-04-05 10:27:31 +0300

    feat: try using CTE

--- models/contact_views/raw_contacts.sql
+++ models/contact_views/raw_contacts.sql
@@ -1,16 +1,20 @@
 {{ 
-      config(
-            materialized='incremental',
-      )
+  config(
+    materialized='incremental',
+  )
  }}
 
-SELECT
-      couchdb.doc,
-      couchdb."@timestamp"
-FROM {{ ref("couchdb") }}
-WHERE (couchdb.doc ->> 'type'::text) = ANY
-      (ARRAY ['contact'::text, 'clinic'::text, 'district_hospital'::text, 'health_center'::text, 'person'::text])
+WITH recent_data AS (
+  SELECT *
+  FROM {{ ref("couchdb") }}
+  {% if is_incremental() %}
+    WHERE couchdb."@timestamp" >= (SELECT MAX(couchdb."@timestamp") FROM {{ this }})
+  {% endif %}
+)
 
-{% if is_incremental() %}
-  AND couchdb."@timestamp" >= (SELECT MAX(couchdb."@timestamp") FROM {{ this }})
-{% endif %}
+SELECT
+  couchdb.doc,
+  couchdb."@timestamp"
+FROM recent_data 
+WHERE (couchdb.doc ->> 'type'::text) = ANY
+  (ARRAY ['contact'::text, 'clinic'::text, 'district_hospital'::text, 'health_center'::text, 'person'::text])
