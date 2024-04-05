b2414fc053fa4b96962b4157b005e01fd1732262
Njuguna Ndung'u <njuguna@medic.org>
2024-04-05 16:34:24 +0300

    chore: try using reported date

--- models/root/couchdb.sql
+++ models/root/couchdb.sql
@@ -6,9 +6,10 @@
 
 SELECT
     doc->>'type' AS type,
+    doc->>'reported_date' AS reported_date,
     *
 FROM v1.{{ env_var('POSTGRES_TABLE') }}
 
 {% if is_incremental() %}
-    WHERE "@timestamp" >= (SELECT MAX("@timestamp") FROM {{ this }})
+    WHERE "reported_date" >= (SELECT MAX("reported_date") FROM {{ this }})
 {% endif %}
