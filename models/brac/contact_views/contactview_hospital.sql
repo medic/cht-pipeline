03c9ba0818e2a59f48f1830f1ba821b6cbd05ca7
Njuguna Ndung'u <njuguna@medic.org>
2024-04-05 12:58:05 +0300

    feat: contactview_hospital as incremental

--- models/brac/contact_views/contactview_hospital.sql
+++ models/brac/contact_views/contactview_hospital.sql
@@ -1,4 +1,16 @@
-SELECT cmd.uuid,
-    cmd.name
-   FROM {{ ref("contactview_metadata") }} cmd
-  WHERE cmd.type = 'district_hospital'::text
+{{
+  config(
+    materialized = 'incremental',
+  )
+}}
+
+SELECT
+  cmd.uuid,
+  cmd.name,
+  cmd."@timestamp"
+FROM {{ ref("contactview_metadata") }} cmd
+WHERE cmd.type = 'district_hospital'::text
+
+{% if is_incremental() %}
+  WHERE "@timestamp" >= (SELECT MAX("@timestamp") FROM {{ this }})
+{% endif %}
