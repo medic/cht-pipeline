b75e0891f578d4b92385ec872674c5bbba44beea
Njuguna Ndung'u <njuguna@medic.org>
2024-05-02 12:10:49 +0300

    feat: data_record as view

--- models/types/data_record.sql
+++ models/types/data_record.sql
@@ -1,27 +1,24 @@
 {{
-    config(
-        materialized = 'incremental',
-        indexes=[
-            {'columns': ['"@timestamp"'], 'type': 'brin'},
-            {'columns': ['"form"'], 'type': 'hash'},
-        ]
-    )
+  config(
+    materialized = 'view',
+    indexes=[
+      {'columns': ['"@timestamp"'], 'type': 'brin'},
+      {'columns': ['"form"'], 'type': 'hash'},
+    ]
+  )
 }}
 
 SELECT
-    doc->>'reported_date' AS reported_date,
-    doc->>'patient_id' AS patient_id,
-    type,
-    form,
-    _id,
-    _rev,
-    doc,
-    doc_as_upsert,
-    "@timestamp",
-    "@version"
+  doc->>'reported_date' AS reported_date,
+  doc->>'patient_id' AS patient_id,
+  type,
+  form,
+  _id,
+  _rev,
+  doc,
+  doc_as_upsert,
+  "@timestamp",
+  "@version"
 FROM {{ ref('couchdb') }}
 WHERE
-    type = 'data_record'
-{% if is_incremental() %}
-    AND COALESCE("@timestamp" > (SELECT MAX("@timestamp") FROM {{ this }}), True)
-{% endif %}
+  type = 'data_record'
