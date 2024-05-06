2ccc6707b8f5e4ed704fa1ad439e2106e95a1fe9
Njuguna Ndung'u <njuguna@medic.org>
2024-05-06 11:47:19 +0300

    fix: hanging comma

--- models/brac/contact_views/contactview_person_fields.sql
+++ models/brac/contact_views/contactview_person_fields.sql
@@ -1,7 +1,14 @@
 {{
   config(
     materialized = 'view',
-  ),
+    indexes=[
+      {'columns': ['uuid'], 'type': 'hash'},
+      {'columns': ['parent_type'], 'form': 'hash'},
+      {'columns': ['reported'], 'form': 'brin'},
+      {'columns': ['"@timestamp"'], 'type': 'brin'},
+      {'columns': ['_id', '_rev'], 'unique': True},
+    ]
+  )
 }}
 
 SELECT
