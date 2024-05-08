91afb612772eb8a48dc708da2a8aadae1e9853a5
Njuguna Ndung'u <njuguna@medic.org>
2024-05-08 16:18:25 +0300

    feat: update references

--- models/contact_views/chp_with_branches.sql
+++ models/contact_views/chp_with_branches.sql
@@ -9,14 +9,14 @@ SELECT
   contactview_chp.branch_uuid as Branch_ID,contactview_chp.phone as Phone, contactview_chp.phone2 as Phone2,
   contactview_chp.date_of_birth as DOB, contactview_chp.parent_type as Parent_Type, contactview_chp.area_uuid as Area_ID,        contactview_metadata.name AS supervisor_name
 FROM 
-  {{ ref("contactview_chp") }},
-  {{ ref("contactview_branch") }},
-  {{ ref("contactview_metadata") }},
+  {{ ref("contactview_chp") }} AS chp,
+  {{ ref("contactview_branch") }} AS branch,
+  {{ ref("contactview_metadata") }} AS metadata,
   {{ ref("contactview_metadata") }} cm
 WHERE
-  contactview_chp.branch_uuid = contactview_branch.uuid AND
-  contactview_chp.supervisor_uuid = contactview_metadata.uuid AND
-  contactview_chp.uuid = cm.contact_uuid AND
-  contactview_branch.name != 'HQ' AND contactview_branch.name != 'HQ OVC'
+  chp.branch_uuid = branch.uuid AND
+  chp.supervisor_uuid = metadata.uuid AND
+  chp.uuid = cm.contact_uuid AND
+  branch.name != 'HQ' AND branch.name != 'HQ OVC'
 ORDER BY
   CHP_Name
