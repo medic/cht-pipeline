2996280ee8b274804e3ea68bd5dbabfe9f14dc9b
Maria Lorena Rodriguez Viruel <marialorenarodriguezviruel@MacBook-Pro-de-Maria.local>
2024-05-08 18:09:51 -0400

    Add incremental filter logic

--- models/contact_views/chp_with_branches.sql
+++ models/contact_views/chp_with_branches.sql
@@ -4,7 +4,7 @@
   )
 }}
 
-SELECT 
+SELECT
   chp.name as CHP_Name,
   chp.uuid as CHP_ID,
   branch.name as Branch_Name,
@@ -14,8 +14,9 @@ SELECT
   chp.date_of_birth as DOB,
   chp.parent_type as Parent_Type,
   chp.area_uuid as Area_ID,
-  metadata.name AS supervisor_name
-FROM 
+  metadata.name as supervisor_name,
+  metadata.reported as reported
+FROM
   {{ ref("contactview_chp") }} AS chp,
   {{ ref("contactview_branch") }} AS branch,
   {{ ref("contactview_metadata") }} AS metadata,
@@ -25,5 +26,8 @@ WHERE
   chp.supervisor_uuid = metadata.uuid AND
   chp.uuid = cm.contact_uuid AND
   branch.name != 'HQ' AND branch.name != 'HQ OVC'
+{% if is_incremental() %}
+    AND metadata.reported > (SELECT MAX(reported) FROM {{ this }}), True)
+{% endif %}
 ORDER BY
   CHP_Name
