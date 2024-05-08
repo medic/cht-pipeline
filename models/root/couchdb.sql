1dc8538adbae2b1bed09e4e02f3512792468ccdd
Maria Lorena Rodriguez Viruel <marialorenarodriguezviruel@MacBook-Pro-de-Maria.local>
2024-05-08 19:02:49 -0400

    Add COALESCE

--- models/root/couchdb.sql
+++ models/root/couchdb.sql
@@ -32,5 +32,5 @@ SELECT
     *
 FROM v1.{{ env_var('POSTGRES_TABLE') }}
 {% if is_incremental() %}
-    WHERE "@timestamp" >= (SELECT MAX("@timestamp") FROM {{ this }})
+  WHERE COALESCE("@timestamp" > (SELECT MAX("@timestamp") FROM {{ this }}), True)
 {% endif %}
