8a5962b7b004ab1653d10c8f4667804dbef05969
Njuguna Ndung'u <njuguna@medic.org>
2024-05-23 12:13:34 +0300

    feat: filter by person early

--- models/contact_views/contactview_person_fields.sql
+++ models/contact_views/contactview_person_fields.sql
@@ -10,12 +10,23 @@
   )
 }}
 
+WITH filtered_person AS (
+  SELECT
+    uuid,
+    phone,
+    phone2,
+    date_of_birth,
+    parent_uuid
+  FROM {{ ref("contact") }}
+  WHERE type = 'person'
+)
+
 SELECT
   person.uuid,
   person.phone,
   person.phone2,
   person.date_of_birth,
   parent.type AS parent_type
-FROM {{ ref("contact") }} AS person
-LEFT JOIN {{ ref("contact") }} AS parent ON person.parent_uuid = parent.uuid
-WHERE person.type = 'person'
+FROM filtered_person AS person
+LEFT JOIN {{ ref("contact") }} AS parent
+ON person.parent_uuid = parent.uuid;
