db585f95a03462dbfb1da4701092978d77856cf3
Njuguna Ndung'u <njuguna@medic.org>
2024-05-23 11:31:07 +0300

    feat: using the mat views causes aa cyclical dep error

--- models/latest_timestamp_views/latest_couchdb_timestamp.sql
+++
@@ -1,7 +0,0 @@
-{{
-  config(
-    materialized = 'materialized_view'
-  )
-}}
-
-SELECT coalesce(max("@timestamp"), '1900-01-01') AS max_timestamp FROM {{ ref('couchdb') }}
--- models/root/couchdb.sql
+++ models/root/couchdb.sql
@@ -13,11 +13,6 @@
   )
 }}
 
-WITH latest_timestamp AS (
-  SELECT max_timestamp
-  FROM {{ ref('latest_couchdb_timestamp') }}
-)
-
 SELECT
   doc ->> '_id'::text AS uuid,
   doc ->> 'type'::text AS type,
@@ -37,5 +32,5 @@ SELECT
   *
 FROM v1.{{ env_var('POSTGRES_TABLE') }}
 {% if is_incremental() %}
-  WHERE "@timestamp" >= latest_timestamp.max_timestamp
+  WHERE "@timestamp" >= {{ max_existing_timestamp('"@timestamp"') }}
 {% endif %}
