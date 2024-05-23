a2a8c8e51deaba2b17899ad20c795b1932d8792c
Njuguna Ndung'u <njuguna@medic.org>
2024-05-23 18:55:04 +0300

    fix: issue with new tables confirmed

--- models/contacts/contact.sql
+++ models/contacts/contact.sql
@@ -35,5 +35,5 @@ FROM {{ ref('couchdb') }}
 WHERE type = ANY
   (ARRAY ['contact'::text, 'clinic'::text, 'district_hospital'::text, 'health_center'::text, 'person'::text])
 {% if is_incremental() %}
-  AND "@timestamp" >= (select coalesce(max("@timestamp"), '1900-01-01') from {{ this }})
+  AND "@timestamp" > (select max("@timestamp") from {{ this }})
 {% endif %}
