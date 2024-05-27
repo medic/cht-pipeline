bb14e0089e3ec25e9365e249cb1dba867fc2a311
Njuguna Ndung'u <njuguna@medic.org>
2024-05-27 11:00:10 +0300

    feat: add chp_with_branches

---
+++ models/brac/contact_views/chp_with_branches.sql
@@ -0,0 +1,33 @@
+{{
+  config(
+    materialized = 'incremental',
+  )
+}}
+
+SELECT
+  chp.name as CHP_Name,
+  chp.uuid as CHP_ID,
+  branch.name as Branch_Name,
+  chp.branch_uuid as Branch_ID,
+  chp.phone as Phone,
+  chp.phone2 as Phone2,
+  chp.date_of_birth as DOB,
+  chp.parent_type as Parent_Type,
+  chp.area_uuid as Area_ID,
+  metadata.name as supervisor_name,
+  metadata.reported as reported
+FROM
+  {{ ref("contactview_chp") }} AS chp,
+  {{ ref("contactview_branch") }} AS branch,
+  {{ ref("contact") }} AS metadata,
+  {{ ref("contact") }} cm
+WHERE
+  chp.branch_uuid = branch.uuid AND
+  chp.supervisor_uuid = metadata.uuid AND
+  chp.uuid = cm.contact_uuid AND
+  branch.name != 'HQ' AND branch.name != 'HQ OVC'
+  {% if is_incremental() %}
+    AND metadata.reported >= (SELECT MAX(reported) FROM {{ this }} WHERE reported IS NOT NULL)
+  {% endif %}
+ORDER BY
+  CHP_Name
