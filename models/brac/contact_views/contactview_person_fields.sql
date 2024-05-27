1c57963ec49fcb681b704223456b90c3ecd85c66
Njuguna Ndung'u <njuguna@medic.org>
2024-05-27 18:17:22 +0300

    fix: update another reference

--- models/brac/contact_views/contactview_person_fields.sql
+++ models/brac/contact_views/contactview_person_fields.sql
@@ -22,7 +22,7 @@ SELECT
   person.phone2,
   person.date_of_birth,
   parent.type AS parent_type,
-  person."@timestamp"
+  person."@timestamp" AS "@timestamp"
 FROM filtered_person AS person
 LEFT JOIN {{ ref("contact") }} AS parent
 ON person.parent_uuid = parent.uuid
