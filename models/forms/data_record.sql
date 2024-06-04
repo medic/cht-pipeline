a19ab1f1990bd9b9af369b7be146ff9e84831256
Njuguna Ndung'u <njuguna@medic.org>
2024-06-04 12:20:34 +0300

    feat: add data_record

---
+++ models/forms/data_record.sql
@@ -0,0 +1,31 @@
+{{
+  config(
+    materialized = 'incremental',
+    unique_key='uuid',
+    indexes=[
+      {'columns': ['"uuid"'], 'type': 'hash'},
+      {'columns': ['"@timestamp"'], 'type': 'brin'},
+      {'columns': ['"reported"'], 'type': 'brin'},
+      {'columns': ['"patient_id"'], 'type': 'hash'},
+      {'columns': ['"contact_uuid"'], 'type': 'hash'},
+      {'columns': ['"contact_parent_uuid"'], 'type': 'hash'},
+      {'columns': ['"form"'], 'type': 'hash'},
+    ]
+  )
+}}
+
+SELECT
+  uuid,
+  (doc #>> '{fields,patient_id}')::text AS patient_id,
+  (doc #>> '{contact,_id}')::text as contact_uuid,
+  (doc #>> '{contact,parent,_id}')::text as contact_parent_uuid,
+  to_timestamp((NULLIF(doc ->> 'reported_date'::text, ''::text)::bigint / 1000)::double precision) AS reported,
+  doc ->> 'form' as form,
+  doc,
+  "@timestamp"
+FROM {{ ref('couchdb') }}
+WHERE
+  type = 'data_record'
+{% if is_incremental() %}
+  AND "@timestamp" >= {{ max_existing_timestamp('"@timestamp"') }}
+{% endif %}
