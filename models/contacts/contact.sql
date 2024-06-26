5e56a41f82f5f370aaac537c811d2248059986e2
Njuguna Ndung'u <njuguna@medic.org>
2024-06-26 18:12:14 +0300

    chore: more testing

--- models/contacts/contact.sql
+++ models/contacts/contact.sql
@@ -5,7 +5,7 @@
     on_schema_change='append_new_columns',
     indexes=[
       {'columns': ['"uuid"'], 'type': 'hash'},
-      {'columns': ['"@timestamp"']},
+      {'columns': ['savedTimestamp']},
       {'columns': ['reported']},
       {'columns': ['parent_uuid']},
       {'columns': ['contact_type']},
@@ -28,5 +28,5 @@ SELECT
 FROM {{ env_var('POSTGRES_SCHEMA') }}.{{ env_var('POSTGRES_TABLE') }}
 WHERE doc->>'type' IN ('contact', 'clinic', 'district_hospital', 'health_center', 'person')
 {% if is_incremental() %}
-  AND "@timestamp" >= {{ max_existing_timestamp('savedTimestamp') }}
+  AND "@timestamp" >= {{ max_existing_timestamp('@timestamp') }}
 {% endif %}
