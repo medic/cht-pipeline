c9c4df8054fe4748048fc7506aa1b37a1170bbf5
Njuguna Ndung'u <njuguna@medic.org>
2024-07-31 18:19:28 +0300

    feat: add tests for checking deleted docs

--- test/sqltest/contact.sql
+++ test/sqltest/contact.sql
@@ -5,10 +5,17 @@ WHERE
   couchdb.doc->>'type' IN ('contact', 'clinic', 'district_hospital', 'health_center', 'person')
   -- TEST CONDITIONS
   AND (
-    -- in couchdb, not in contact table
-    (contact.uuid IS NULL)
-    OR -- fields dont match
-    contact.parent_uuid <> couchdb.doc->'parent'->>'_id' OR
-    contact.contact_type <> COALESCE(couchdb.doc->>'contact_type', couchdb.doc->>'type') OR
-    contact.phone <> couchdb.doc->>'phone'
+    (
+      -- in couchdb, not in contact table
+      (contact.uuid IS NULL)
+      OR -- fields dont match
+      contact.parent_uuid <> couchdb.doc->'parent'->>'_id' OR
+      contact.contact_type <> COALESCE(couchdb.doc->>'contact_type', couchdb.doc->>'type') OR
+      contact.phone <> couchdb.doc->>'phone'
+    )
+    OR 
+    (
+      -- contact has deleted flag
+      couchdb.doc->>'_deleted' = 'true'
+    )
   )
--- test/sqltest/data_record.sql
+++ test/sqltest/data_record.sql
@@ -5,11 +5,18 @@ WHERE
   couchdb.doc->>'type' = 'data_record'
   -- TEST CONDITIONS
   AND (
-    -- in couchdb, not in data_record
-    (data_record.uuid IS NULL)
-    OR -- fields dont match
-    data_record.from_phone <> couchdb.doc->>'from' OR
-    data_record.form <> couchdb.doc->>'form' OR
-    data_record.patient_id <> couchdb.doc->>'patient_id' OR
-    data_record.contact_uuid <> couchdb.doc->'contact'->>'_id'
+    (
+      -- in couchdb, not in data_record
+      (data_record.uuid IS NULL)
+      OR -- fields dont match
+      data_record.from_phone <> couchdb.doc->>'from' OR
+      data_record.form <> couchdb.doc->>'form' OR
+      data_record.patient_id <> couchdb.doc->>'patient_id' OR
+      data_record.contact_uuid <> couchdb.doc->'contact'->>'_id'
+    )
+    OR 
+    (
+      -- contact has deleted flag
+      couchdb.doc->>'_deleted' = 'true'
+    )
   )
