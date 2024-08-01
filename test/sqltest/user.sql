8244f6a17258ad62ab5e22c0fb970dc3c620dc50
Tom Wier <thomas@medic.org>
2024-08-01 11:55:23 +0300

    feat: deletion changes for users

---
+++ test/sqltest/user.sql
@@ -0,0 +1,13 @@
+SELECT
+FROM {{ env_var('POSTGRES_SCHEMA') }}.{{ env_var('POSTGRES_TABLE') }} couchdb
+LEFT JOIN {{ ref('user') }} cht_user ON couchdb._id = cht_user.user_id
+WHERE
+  couchdb.doc->>'type' = 'user-settings'
+  -- TEST CONDITIONS
+  AND (
+    -- in couchdb, not deleted, but not in user table
+    (cht_user.user_id IS NULL AND couchdb._deleted = false)
+    OR
+    -- in couchdb, deleted, but still in user table
+    (cht_user.user_id IS NOT NULL AND couchdb._deleted = true)
+  )
