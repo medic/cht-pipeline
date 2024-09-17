7babb396bc110889b14ff2d9f5ca0bea02eaff3f
njuguna-n <141340177+njuguna-n@users.noreply.github.com>
2024-09-17 16:28:16 +0300

    fix: fail builds if dbt run fails

--- tests/run_dbt_tests_docker.sh
+++ tests/run_dbt_tests_docker.sh
@@ -3,6 +3,10 @@ echo Install dbt dependencies ...
 dbt deps
 echo Running dbt ...
 dbt run
+run_exit_status=$?
+if [ $run_exit_status -ne 0 ]; then
+  echo "DBT run failed"
+  exit $run_exit_status
+fi
 echo Running tests ...
 dbt test
-
