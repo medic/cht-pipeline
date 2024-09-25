a5c4bc0fd7113efe5c36fef1eb205ba5c36e3c46
Maria Lorena Rodriguez Viruel <marialorenarodriguezviruel@mbp-de-maria.lan>
2024-09-25 14:40:28 -0400

    Update custom date validation test to handle type casting

--- tests/generic/assert_date_format.sql
+++ tests/generic/assert_date_format.sql
@@ -3,23 +3,23 @@
   FROM {{ model }}
   WHERE {{ column_name }} IS NOT NULL
     AND (
-      NOT ({{ column_name }} ~ '^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$')
+      NOT (CAST({{ column_name }} AS TEXT) ~ '^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$')
       OR NOT (
-        TO_DATE({{ column_name }}, 'YYYY-MM-DD') = {{ column_name }}
+        TO_DATE(CAST({{ column_name }} AS TEXT), 'YYYY-MM-DD') = CAST({{ column_name }} AS TEXT)
         AND (
-          (EXTRACT(MONTH FROM TO_DATE({{ column_name }}, 'YYYY-MM-DD')) != 2)
-          OR (EXTRACT(DAY FROM TO_DATE({{ column_name }}, 'YYYY-MM-DD')) <=
+          (EXTRACT(MONTH FROM TO_DATE(CAST({{ column_name }} AS TEXT), 'YYYY-MM-DD')) != 2)
+          OR (EXTRACT(DAY FROM TO_DATE(CAST({{ column_name }} AS TEXT), 'YYYY-MM-DD')) <=
               CASE
-                WHEN EXTRACT(YEAR FROM TO_DATE({{ column_name }}, 'YYYY-MM-DD')) % 4 = 0
-                  AND (EXTRACT(YEAR FROM TO_DATE({{ column_name }}, 'YYYY-MM-DD')) % 100 != 0
-                  OR EXTRACT(YEAR FROM TO_DATE({{ column_name }}, 'YYYY-MM-DD')) % 400 = 0) THEN 29
+                WHEN EXTRACT(YEAR FROM TO_DATE(CAST({{ column_name }} AS TEXT), 'YYYY-MM-DD')) % 4 = 0
+                  AND (EXTRACT(YEAR FROM TO_DATE(CAST({{ column_name }} AS TEXT), 'YYYY-MM-DD')) % 100 != 0
+                  OR EXTRACT(YEAR FROM TO_DATE(CAST({{ column_name }} AS TEXT), 'YYYY-MM-DD')) % 400 = 0) THEN 29
                 ELSE 28
               END
           )
         )
         AND NOT (
-          (EXTRACT(MONTH FROM TO_DATE({{ column_name }}, 'YYYY-MM-DD')) IN (4, 6, 9, 11))
-          AND EXTRACT(DAY FROM TO_DATE({{ column_name }}, 'YYYY-MM-DD')) > 30
+          (EXTRACT(MONTH FROM TO_DATE(CAST({{ column_name }} AS TEXT), 'YYYY-MM-DD')) IN (4, 6, 9, 11))
+          AND EXTRACT(DAY FROM TO_DATE(CAST({{ column_name }} AS TEXT), 'YYYY-MM-DD')) > 30
         )
       )
     )
