b5ca26b7995703cd63bcdd42eca8f37794a80579
Njuguna Ndung'u <njuguna@medic.org>
2024-11-14 11:11:38 +0300

    feat: add feedback and telemetry tables

---
+++ models/users/feedback.sql
@@ -0,0 +1,32 @@
+{{
+  config(
+    materialized = 'incremental',
+    unique_key='uuid',
+    on_schema_change='append_new_columns',
+    indexes=[
+      {'columns': ['uuid'], 'type': 'hash'},
+      {'columns': ['saved_timestamp']},
+      {'columns': ['period_start']},
+    ]
+  )
+}}
+
+SELECT
+  document_metadata.uuid as uuid,
+  document_metadata.saved_timestamp,
+  doc#>>'{meta,source}' AS SOURCE,    
+  doc#>>'{meta,url}' AS url,
+  doc#>>'{meta,user,name}' AS user_name,
+  doc#>>'{meta,time}' AS period_start,
+  COALESCE(doc#>>'{info,cause}',doc->>'info') AS cause,
+  doc#>>'{info,message}' AS message
+FROM {{ ref('document_metadata') }} document_metadata
+INNER JOIN
+  {{ source('couchdb', env_var('POSTGRES_TABLE')) }} source_table
+  ON source_table._id = document_metadata.uuid
+WHERE
+  document_metadata.doc_type = 'feedback'
+  AND document_metadata._deleted = false
+{% if is_incremental() %}
+  AND document_metadata.saved_timestamp >= {{ max_existing_timestamp('saved_timestamp') }}
+{% endif %}
