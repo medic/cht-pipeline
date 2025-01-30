9c23188954aa8eae4af04663cf2bab6542b44165
Tom Wier <thomas@medic.org>
2025-01-30 12:58:02 +0300

    feat(#172): add instance and dbname

--- models/root/document_metadata.sql
+++ models/root/document_metadata.sql
@@ -9,6 +9,8 @@
       {'columns': ['saved_timestamp']},
       {'columns': ['doc_type']},
       {'columns': ['_deleted']},
+      {'columns': ['isntance']},
+      {'columns': ['dbname']},
     ]
   )
 }}
@@ -17,7 +19,9 @@ SELECT
   _id as uuid,
   _deleted,
   saved_timestamp,
-  doc->>'type' as doc_type
+  doc->>'type' as doc_type,
+  split_part(source, '/', 1) AS instance,
+  split_part(source, '/', 2) AS dbname
 from {{ source('couchdb', env_var('POSTGRES_TABLE')) }} source_table
 {% if is_incremental() %}
 WHERE source_table.saved_timestamp >= {{ max_existing_timestamp('saved_timestamp') }}
