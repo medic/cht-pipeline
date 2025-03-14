e5109f6f2fc0be48ba94d1e227cf16ddf30c8794
Tom Wier <thomas@medic.org>
2025-03-14 11:50:17 +0300

    fix: changing how batching works

--- macros/batch_incremental.sql
+++ macros/batch_incremental.sql
@@ -1,38 +1,30 @@
 -- for incremental runs
--- select ALL records from source where document_metadata.saved_timestamp
--- is exactly equal to source.saved_timestamp
--- then union this with at most batch_size records
--- where document_metadata.source_timestamp is strictly greater than
--- document_metadata.saved_timestamp
--- this ensures that all records with the same timestamp are inserted
--- but limits later records to batch_size
+-- assume all in the source table with timestamp <= the max timestamp in the target table
+-- have already been inserted
+-- select up to batch_size rows from the source table where timestamp > max_timestamp, 
+-- ordered by timestamp
+-- then union with all all rows in the source table that have the same timestamp
+-- as the maximum timestamp in the btach to be inserted
+-- to ensure that all rows with the same timestamp are inserted in the same batch
 {% macro batch_incremental(source_cte_name) %}
-  {% if is_incremental() %}
     --define a CTE current_batch with a limit of batch_size
     , current_batch AS (
       SELECT * FROM {{ source_cte_name }}
+
+			{% if is_incremental() %}
       WHERE saved_timestamp > {{ max_existing_timestamp('saved_timestamp') }}
+			{% endif %}
+
       {% if var("batch_size", none) is not none %}
         ORDER BY saved_timestamp
         LIMIT {{ var('batch_size') }}
       {% endif %}
     )
 
-    -- union the CTE with a query getting all records with equal timestamp
+    -- union the CTE with a query getting all records with highest timestamp in current_batch
     SELECT * FROM {{ source_cte_name }}
-    WHERE saved_timestamp = {{ max_existing_timestamp('saved_timestamp') }}
-    UNION ALL
+    WHERE saved_timestamp = (SELECT MAX(saved_timestamp) FROM current_batch)
+    UNION
     SELECT * FROM current_batch
 
-  -- if not incremental (the table is being created for the first time)
-  -- and batch size is defined
-  -- apply a limit of batch size so the entire table is not created
-  -- in a single batch
-  {% else %}
-    SELECT * FROM {{ source_cte_name }}
-    {% if var("batch_size", none) is not none %}
-      ORDER BY saved_timestamp
-      LIMIT {{ var('batch_size') }}
-    {% endif %}
-  {% endif %}
 {% endmacro %}
