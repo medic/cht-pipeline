db88e79f1dd17e6fc670299a7a5c9ef673d24abc
Tom Wier <thomas@medic.org>
2025-06-20 15:52:58 +0300

    feat: testing removing CTE from batching

--- models/root/document_metadata.sql
+++ models/root/document_metadata.sql
@@ -15,15 +15,49 @@
   )
 }}
 
-WITH source_records AS (
-  SELECT
-    _id as uuid,
-    _deleted,
-    saved_timestamp,
-    doc->>'type' as doc_type,
+{% if is_incremental() %}
+{% if var("batch_size", none) is not none %}
+( 
+SELECT
+  _id AS uuid,
+  _deleted,
+  saved_timestamp,
+  doc->>'type' AS doc_type,
+  split_part(source, '/', 1) AS instance,
+  split_part(source, '/', 2) AS dbname
+FROM {{ source('couchdb', env_var('POSTGRES_TABLE')) }} source_table
+WHERE saved_timestamp > {{ max_existing_timestamp('saved_timestamp') }}
+ORDER BY saved_timestamp
+LIMIT {{ var('batch_size') }}
+) UNION (
+SELECT
+  _id AS uuid,
+  _deleted,
+  saved_timestamp,
+  doc->>'type' AS doc_type,
   split_part(source, '/', 1) AS instance,
   split_part(source, '/', 2) AS dbname
   FROM {{ source('couchdb', env_var('POSTGRES_TABLE')) }} source_table
+  WHERE saved_timestamp = {{ max_existing_timestamp('saved_timestamp') }}
 )
-
-{{ batch_incremental('source_records') }}
+{% else %}
+SELECT
+  _id AS uuid,
+  _deleted,
+  saved_timestamp,
+  doc->>'type' AS doc_type,
+  split_part(source, '/', 1) AS instance,
+  split_part(source, '/', 2) AS dbname
+  FROM {{ source('couchdb', env_var('POSTGRES_TABLE')) }} source_table
+  WHERE saved_timestamp >= {{ max_existing_timestamp('saved_timestamp') }}
+{% endif %}
+{% else %}
+SELECT
+  _id AS uuid,
+  _deleted,
+  saved_timestamp,
+  doc->>'type' AS doc_type,
+  split_part(source, '/', 1) AS instance,
+  split_part(source, '/', 2) AS dbname
+  FROM {{ source('couchdb', env_var('POSTGRES_TABLE')) }} source_table
+{% endif %}
